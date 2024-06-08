53b076164f9c04224c1152e22dd51f6398b6d089
sauergru <sauergru@gmail.com>
2024-06-08 22:50:47 +0200

    added token + navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
+import Navigation from './components/Navigation/Navigation';
 
-const HomePage = lazy(() => import('./pages/HomePage'));
-const MoviesPage = lazy(() => import('./pages/MoviesPage'));
-const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'));
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+// const HomePage = lazy(() => import('./pages/HomePage'));
+// const MoviesPage = lazy(() => import('./pages/MoviesPage'));
+// const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'));
+// const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+// const MovieCast = lazy(() => import('./components/MovieCast'));
+// const MovieReviews = lazy(() => import('./components/MovieReviews'));
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Navigation />
+      <Suspense fallback={<div>Loading...</div>}>
+        {/* <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes> */}
+      </Suspense>
     </div>
   );
 };
